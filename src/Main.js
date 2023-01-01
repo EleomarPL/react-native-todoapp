bb396b7f5f995813366d5a5145b1a859c082b710
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2023-01-01 10:11:15 -0600

    add: Main index file was added

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Hola mundo desde React Native</Text>
+    </View>
+  )
+}
+
+export default Main
