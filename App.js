614e5960fec76169b3fc915b7d482825ad048dce
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2023-01-01 10:12:29 -0600

    add: Set app react native with base structure

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
 
-export default function App() {
+import Main from './src/Main'
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={ styles.container }>
+      <Main />
       <StatusBar style="auto" />
-    </View>
-  );
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: 'red',
+    maxHeight: '95%',
+    overflow: 'hidden',
+    marginTop: Constants.statusBarHeight
+  }
+})
